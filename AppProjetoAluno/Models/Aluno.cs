84e53e396504a4a8e415804edc61d2b171e75aee
2019102049035-5 <2019102049035-5@JIPA-LABIN03-28>
2024-02-27 21:12:55 -0400

    Adicionar arquivos de projeto.

---
+++ AppProjetoAluno/Models/Aluno.cs
@@ -0,0 +1,22 @@
+﻿namespace AppProjetoAluno.Models
+{
+    public class Aluno
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public double Nota1 { get; set; }
+        public double Nota2 { get; set; }
+        public double Media { get; set; }
+        public string? Situacao { get; set; }
+
+        public void CalcularMedia()
+        {
+            Situacao = "REPROVADO(A)";
+            Media = (Nota1 + Nota2) / 2;
+            if (Media >= 60)
+            {
+                Situacao = "APROVADO(A)";
+            }
+        }
+    }
+}
